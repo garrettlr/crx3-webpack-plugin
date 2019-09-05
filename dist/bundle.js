a64814fffe39f1318295e3fca9633592e25126c3
Garrett Levy <Garrett@getservice.com>
2019-09-05 11:37:41 -0700

    support all working features of crx3

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,15 +6,28 @@ var path = require('path');
 var path__default = _interopDefault(path);
 var crx3 = _interopDefault(require('crx3'));
 
+const MANIFEST = "manifest.json";
+const NAME = "crx";
+const UPDATE_URL = "http://localhost:8000/";
+const UPDATE_FILENAME = "updates.xml";
+
 class Plugin {
   constructor(options) {
     this.options = options || {};
+    
+    if (!this.options.name) this.options.name = NAME;
+
     if (!this.options.updateUrl) {
-      this.options.updateUrl = "http://localhost:8000/";
+      this.options.updateUrl = UPDATE_URL;
     }
+
     if (!this.options.updateFilename) {
-      this.options.updateFilename = "updates.xml";
+      if (this.options.name) {
+        this.options.updateFilename = `${this.options.name}.xml`;
+      } else this.options.updateFilename = UPDATE_FILENAME;
     }
+    // export a zip of the build files.
+    this.zip = !!this.options.zip;
 
     // remove trailing slash
     this.options.updateUrl = this.options.updateUrl.replace(/\/$/, "");
@@ -24,15 +37,22 @@ class Plugin {
     this.keyFile = path__default.isAbsolute(this.options.keyFile) ? this.options.keyFile : path.join(this.context, this.options.keyFile);
     this.outputPath = path__default.isAbsolute(this.options.outputPath) ? this.options.outputPath : path.join(this.context, this.options.outputPath);
     this.contentPath = path__default.isAbsolute(this.options.contentPath) ? this.options.contentPath : path.join(this.context, this.options.contentPath);
-    this.name = this.options.name ? this.options.name : "extension";
-    // set output info
-    this.crxName = this.options.name + ".crx";
-    this.crxFile = path.join(this.outputPath, this.crxName);
-    this.updateFile = path.join(this.outputPath, this.options.updateFilename);
-    this.updateUrl = this.options.updateUrl + "/" + this.options.updateFilename;
+    this.name = this.options.name;
 
+    // set output info
+    this.crxName = `${this.options.name}.crx`;
+    this.zipName = `${this.options.name}.zip`;
+
+    this.crxFile = path.join(this.outputPath, this.crxName);
+    this.zipFile = path.join(this.outputPath, this.zipName);
+    this.manifest = path.join(this.contentPath, MANIFEST);
+    this.updateFile = path.join(this.outputPath, this.options.updateFilename);
+    this.updateUrl = `${this.options.updateUrl}/${this.options.updateFilename}`;
+    
+    // bind methods
     this.apply = this.apply.bind(this);
     this.package = this.package.bind(this);
+    this.handleOptionalSettings = this.handleOptionalSettings.bind(this);
   }
 
   // hook into webpack
@@ -42,15 +62,25 @@ class Plugin {
     })
   }
 
+  handleOptionalSettings(options) {
+    const { zip, version, browserVersion } = this.options;
+    if (zip) options.zipPath = this.zipFile;
+    // if (version) options.appVersion = version; not working in crx3 at the moment.
+    if (browserVersion) options.browserVersion = browserVersion;
+    return options;
+  }
+
   // package the crx.
   package() {
-    crx3([`${this.contentPath}/manifest.json`], {
+    const options = {
       keyPath: this.keyFile,
-      xmlPath: `${this.outputPath}/${this.updateFilename}`,
+      xmlPath: this.updateFile,
       crxURL: this.updateUrl,
-      crxPath: `${this.outputPath}/${this.name}.crx`,
-    })
-      .then(() => console.log('done'))
+      crxPath: this.crxFile,
+    };
+    this.handleOptionalSettings(options);
+
+    crx3([this.manifest], options)
       .catch(console.error);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
 import path, { join } from 'path';
 import crx3 from 'crx3';
 
+const MANIFEST = "manifest.json";
+const NAME = "crx";
+const UPDATE_URL = "http://localhost:8000/";
+const UPDATE_FILENAME = "updates.xml";
+
 class Plugin {
   constructor(options) {
     this.options = options || {};
+    
+    if (!this.options.name) this.options.name = NAME;
+
     if (!this.options.updateUrl) {
-      this.options.updateUrl = "http://localhost:8000/";
+      this.options.updateUrl = UPDATE_URL;
     }
+
     if (!this.options.updateFilename) {
-      this.options.updateFilename = "updates.xml";
+      if (this.options.name) {
+        this.options.updateFilename = `${this.options.name}.xml`;
+      } else this.options.updateFilename = UPDATE_FILENAME;
     }
+    // export a zip of the build files.
+    this.zip = !!this.options.zip;
 
     // remove trailing slash
     this.options.updateUrl = this.options.updateUrl.replace(/\/$/, "");
@@ -19,15 +32,22 @@ class Plugin {
     this.keyFile = path.isAbsolute(this.options.keyFile) ? this.options.keyFile : join(this.context, this.options.keyFile);
     this.outputPath = path.isAbsolute(this.options.outputPath) ? this.options.outputPath : join(this.context, this.options.outputPath);
     this.contentPath = path.isAbsolute(this.options.contentPath) ? this.options.contentPath : join(this.context, this.options.contentPath);
-    this.name = this.options.name ? this.options.name : "extension";
-    // set output info
-    this.crxName = this.options.name + ".crx";
-    this.crxFile = join(this.outputPath, this.crxName);
-    this.updateFile = join(this.outputPath, this.options.updateFilename);
-    this.updateUrl = this.options.updateUrl + "/" + this.options.updateFilename;
+    this.name = this.options.name
 
+    // set output info
+    this.crxName = `${this.options.name}.crx`;
+    this.zipName = `${this.options.name}.zip`;
+
+    this.crxFile = join(this.outputPath, this.crxName);
+    this.zipFile = join(this.outputPath, this.zipName);
+    this.manifest = join(this.contentPath, MANIFEST);
+    this.updateFile = join(this.outputPath, this.options.updateFilename);
+    this.updateUrl = `${this.options.updateUrl}/${this.options.updateFilename}`;
+    
+    // bind methods
     this.apply = this.apply.bind(this);
     this.package = this.package.bind(this);
+    this.handleOptionalSettings = this.handleOptionalSettings.bind(this);
   }
 
   // hook into webpack
@@ -37,16 +57,26 @@ class Plugin {
     })
   }
 
+  handleOptionalSettings(options) {
+    const { zip, version, browserVersion } = this.options;
+    if (zip) options.zipPath = this.zipFile;
+    // if (version) options.appVersion = version; not working in crx3 at the moment.
+    if (browserVersion) options.browserVersion = browserVersion;
+    return options;
+  }
+
   // package the crx.
   package() {
-    crx3([`${this.contentPath}/manifest.json`], {
+    const options = {
       keyPath: this.keyFile,
-      xmlPath: `${this.outputPath}/${this.updateFilename}`,
+      xmlPath: this.updateFile,
       crxURL: this.updateUrl,
-      crxPath: `${this.outputPath}/${this.name}.crx`,
-    })
-      .then(() => console.log('done'))
-      .catch(console.error)
+      crxPath: this.crxFile,
+    }
+    this.handleOptionalSettings(options);
+
+    crx3([this.manifest], options)
+      .catch(console.error);
   }
 }
 
